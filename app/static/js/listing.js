fd848d7444bfde74c26f8a22edbf809c9c98263d
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2018-11-03 20:29:50 +0200

    refresh requirement.txt

--- app/static/js/listing.js
+++ app/static/js/listing.js
@@ -4,7 +4,7 @@ const jsonStr = $("#varContainer").data("listing").replace(/\'/g, '"');
 var listing = JSON.parse(jsonStr);
 console.log(listing)
 
-const { guests = 1, bedroom = 1, beds = 1, baths = 1, lat = 51.5136, lng = 23.3219, address = 'SOHO', landmarks = [], events = [], rating = 5 } = listing;
+const { guests = 1, bedrooms = 1, beds = 1, baths = 1, lat = 51.5136, lng = 23.3219, address = 'SOHO', landmarks = [], events = [], rating = 5 } = listing;
 
 
 // rating
