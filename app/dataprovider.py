4eca6c2d322ec392f6fd4bc2759cc1900975bb82
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2018-11-04 12:21:59 +0200

    add geocode (here api) for nearby landmarks by the listing

--- app/dataprovider.py
+++ app/dataprovider.py
@@ -1,12 +1,11 @@
 from models import *
-import parser
-
+import requests
+import json
 
 def getListings():
     listings = Listing.query.all()
-    for l in listings:
-        print(l.user)
-    listingsDto = [listingToDto(listing) for listing in listings]
+
+    listingsDto = [listingToDto(listing, False) for listing in listings]
     return listingsDto
 
 def getListingById(id):
@@ -17,11 +16,11 @@ def getListingById(id):
 
 
 
-def listingToDto(listing):
+def listingToDto(listing, checkLandmarks = True):
     imgs = [img.image_path  for img in  listing.images] * 3
 
     lat = float(listing.latitude);
-    lon = float(listing.longitude);
+    lng = float(listing.longitude);
 
     username = ''
     if listing.user is None:
@@ -29,15 +28,24 @@ def listingToDto(listing):
     else:
         username = listing.user.username
 
+    landmarks = [{ "lat": 42.698334, "lng": 23.319941, "title": "Hi" }];
+
+    if checkLandmarks:
+        print('checking landmarks ...')
+        landmarks = getLandmarks(lat, lng)
+    else:
+        print('skipping landmars ...')
+
     dto = {
         "id": listing.id,
         "guests": listing.guests,
         "baths": listing.baths,
         "title": listing.address,
         "price": listing.price,
-        "landmarks": [{ "lat": -80.3888495, "lng": -100.445735, "title": "Hi" }],
+        "landmarks": landmarks,
+        "landmarksCount": len(landmarks),   
         "lat": lat,
-        "lng": lon,
+        "lng": lng,
         "rating": listing.rating,
         "user": {"name": username},
         "images": imgs,
@@ -46,3 +54,45 @@ def listingToDto(listing):
     }
 
     return dto
+
+
+def getLandmarks(lat, lng):
+    
+    defaultLocation = {
+        "lat": 42.698334, 
+        "lng": 23.319941
+    };
+
+    appId = 'wgV16NBsDKmQocrusbbq'
+    appCode = 'ZZOvDZwcmhoKQ-ZqFHmtZg'
+
+    url = "https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?app_id=%s&app_code=%s&mode=retrieveLandmarks&prox=%s,%s,1000" % (appId, appCode, lat, lng)
+
+    r = requests.get(url)
+    dictData = r.json()
+
+    resultsList = dictData["Response"]["View"] 
+    results = []
+    if len(resultsList) > 0:
+        print 
+        results = resultsList[0]["Result"];
+    else:
+        return []
+
+    r = []
+
+    for res in results:
+        lat = res["Location"]["DisplayPosition"]["Latitude"]
+        lng = res["Location"]["DisplayPosition"]["Longitude"]
+        title = res["Location"]["Name"]
+        locationType = res["Location"]["LocationType"]
+
+        mapped = {
+            'lat': lat, 
+            'lng': lng, 
+            'title': title, 
+            'locationType': locationType  
+        }
+        print(mapped)
+        r.append(mapped)
+    return r
--- app/server.py
+++ app/server.py
@@ -19,6 +19,28 @@ db = SQLAlchemy(app)
 import dataprovider
 from models import *
 
+
+@app.route('/listing')
+def details():
+    id = request.args.get('id')
+    listingDto = dataprovider.getListingById(id)
+    return render_template('listing.html', listing = listingDto )
+
+@app.route('/')
+def allListings():
+    listingsDto = dataprovider.getListings()
+    return render_template('listings.html', listings = listingsDto)
+
+@app.route('/createListing')
+def createListing():
+    return render_template('create-listing.html')
+
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 mockListing = {
    "id": randint(1,10),
    "guests": 6,
@@ -34,31 +56,3 @@ mockListing = {
   "amenities": ["test", "test1", "test2", "test3", "test4"],
   "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas justo dolor, interdum quis sodales ut, viverra ut nisl. In tempor sit amet quam non rhoncus. Suspendisse potenti. Cras convallis interdum arcu, eu tincidunt metus lacinia eu. Mauris eleifend pharetra tristique. Sed nisi sapien, laoreet non nunc non, faucibus mattis odio. Fusce ac sodales ex, quis lobortis neque. Ut non ex tellus. Proin tellus massa, condimentum quis dignissim sed, ornare blandit tortor. Phasellus ipsum velit, dignissim egestas metus in, lobortis gravida erat. Aenean vel dui lorem. Curabitur a velit risus. Proin ut malesuada magna. Donec nunc nisi, condimentum pharetra justo porttitor, vehicula fermentum mauris. Pellentesque scelerisque egestas egestas. Curabitur facilisis elit nibh, accumsan porttitor mauris fermentum et. Phasellus ornare molestie tortor quis pretium. Integer elementum arcu in justo efficitur placerat. Donec tincidunt iaculis ante nec dapibus. Fusce eu velit sed sem luctus convallis eu vel ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam sed ante sollicitudin, vestibulum neque id, imperdiet odio. Nulla ut gravida leo. Nam vel efficitur urna. Nam nulla dui, auctor eu molestie non, rutrum sed nisl. Cras ligula nisl, ornare laoreet egestas sit amet, consequat sed lorem. Vestibulum ultrices tristique consectetur."
 }
-
-
-# @app.route('/listings')
-# def listings():
-#     listings = dataprovider.getListings()
-#     return jsonify(len(listings))
-
-@app.route('/listing')
-def details():
-    id = request.args.get('id')
-    listingDto = dataprovider.getListingById(id)
-    print(listingDto)
-    return render_template('listing.html', listing = listingDto )
-
-@app.route('/')
-def allListings():
-    listingsDto = dataprovider.getListings();
-    print(listingsDto)
-    return render_template('listings.html', listings = listingsDto)
-
-@app.route('/createListing')
-def createListing():
-    return render_template('create-listing.html')
-
-
-# If we're running in stand alone mode, run the application
-if __name__ == '__main__':
-    app.run(debug=True)
